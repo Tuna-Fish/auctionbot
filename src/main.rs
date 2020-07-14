e43aa87f6801e0dc23cbd6d46d567bae325cf9a2
Antti-Ville Tuunainen <avtuunainen@gmail.com>
2020-07-14 13:11:35 +0300

    token configuration and client creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod config;
+
+use serenity::{
+    prelude::*,
+    model::prelude::*,
+    Client,
+};
+struct Handler;
+impl EventHandler for Handler {
+    fn message(&self, context: Context, msg: Message) {
+        unimplemented!();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut client = Client::new(config::TOKEN, Handler).expect("couldn't create the new client!");
 }
